5b7a60ff9486b08f4d74491ca5927d6b1aef7a22
joudy-sb <joudy.f.sabbagh@gmail.com>
2025-04-07 23:50:35 +0300

    Load Hugging Face NLLB translation model in Flask app

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 app = Flask(__name__)
+
+# Load the model and tokenizer
+model_name = "facebook/nllb-200-distilled-600M"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 @app.route("/", methods=["GET"])
 def home():
